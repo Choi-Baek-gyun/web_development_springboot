c986416505d484e33c2b2e759afdbcd9d0a17868
chlqorrbs <qorrbs9341@gmail.com>
2025-01-15 21:44:18 +0900

    feat: jwt lesson

--- chapter08/src/main/java/me/qorrbs/springbootdeveloper/controller/UserViewController.java
+++ chapter08/src/main/java/me/qorrbs/springbootdeveloper/controller/UserViewController.java
@@ -1,5 +1,9 @@
 package me.qorrbs.springbootdeveloper.controller;
 
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
@@ -20,4 +24,16 @@ public class UserViewController {
 
         resource/templates에 login.html 생성
      */
+
+    // 로그아웃 관련
+    @GetMapping("/logout")
+    public String logout (HttpServletRequest request, HttpServletResponse response) {
+        new SecurityContextLogoutHandler().logout(request, response,
+                SecurityContextHolder.getContext().getAuthentication());
+
+        return "redirect:/login";
+
+    }
+
+
 }
---
+++ chapter08_modified/src/main/resources/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO article (title, content, created_at, updated_at) VALUES ('title 1', 'content 1', now(), now())
+INSERT INTO article (title, content, created_at, updated_at) VALUES ('title 2', 'content 2', now(), now())
+INSERT INTO article (title, content, created_at, updated_at) VALUES ('title 3', 'content 3', now(), now())
